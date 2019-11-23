5f2105ab8c056e11e6c2018119a245a53f3faee5
Lai Jiewen <alanljw12345@gmail.com>
2019-11-22 17:21:39 -0800

    Add queue detail activity

---
+++ app/src/main/java/tiger/uniqueue/ui/queue/QueueDetailActivity.kt
@@ -0,0 +1,13 @@
+package tiger.uniqueue.ui.queue
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tiger.uniqueue.R
+
+class QueueDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_queue_detail)
+    }
+}
