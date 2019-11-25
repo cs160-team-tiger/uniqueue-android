56c3936c149b92d923fdffdba2637d154b2a4d8a
Lai Jiewen <alanljw12345@gmail.com>
2019-11-25 13:16:11 -0800

    Open the dialog

--- app/src/main/java/tiger/uniqueue/ui/queue/QueueDetailActivity.kt
+++ app/src/main/java/tiger/uniqueue/ui/queue/QueueDetailActivity.kt
@@ -1,7 +1,11 @@
 package tiger.uniqueue.ui.queue
 
+import android.app.Dialog
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,12 +13,14 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import butterknife.BindView
 import butterknife.ButterKnife
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import tiger.uniqueue.R
 import tiger.uniqueue.data.Resource
 import tiger.uniqueue.data.model.Question
 import tiger.uniqueue.data.model.Queue
 import tiger.uniqueue.onError
 import java.util.*
+
 
 class QueueDetailActivity : AppCompatActivity() {
 
@@ -26,6 +32,8 @@ class QueueDetailActivity : AppCompatActivity() {
     lateinit var queueHeaderList: RecyclerView
     @BindView(R.id.rv_question_list)
     lateinit var questionListView: RecyclerView
+    @BindView(R.id.fab_add)
+    lateinit var addQuestionButon: FloatingActionButton
 
     private lateinit var headerAdapter: QueueAdapter
     private lateinit var questionAdapter: QuestionAdapter
@@ -53,6 +61,10 @@ class QueueDetailActivity : AppCompatActivity() {
 
         swipeRefresh.setOnRefreshListener(this::fetchQueue)
 
+        addQuestionButon.setOnClickListener {
+            openAddQuestionDialog()
+        }
+
         viewModel.queue.observe(this, Observer {
             when (it) {
                 is Resource.Success -> {
@@ -70,10 +82,12 @@ class QueueDetailActivity : AppCompatActivity() {
             when (it) {
                 is Resource.Success -> {
                     swipeRefresh.isRefreshing = false
+                    addQuestionButon.isVisible = true
                     updateQuestionList(it.data!!)
                 }
                 is Resource.Loading -> {
                     swipeRefresh.isRefreshing = true
+                    addQuestionButon.isVisible = false
                 }
                 is Resource.Error -> {
                     swipeRefresh.isRefreshing = false
@@ -81,6 +95,18 @@ class QueueDetailActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun openAddQuestionDialog() {
+        val ft = supportFragmentManager.beginTransaction()
+        val prev = supportFragmentManager.findFragmentByTag("dialog")
+        if (prev != null) {
+            ft.remove(prev)
+        }
+        ft.addToBackStack(null)
+        val newFragment =
+            AddQuestionDialogFragment.newInstance(queueId)
+        newFragment.show(ft, "dialog")
     }
 
     private fun fetchQueue() {
@@ -97,6 +123,47 @@ class QueueDetailActivity : AppCompatActivity() {
 
     private fun updateQuestionList(questions: List<Question>) {
         questionAdapter.setNewData(questions)
+    }
+
+    class AddQuestionDialogFragment : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            return activity?.let {
+                val builder = AlertDialog.Builder(it)
+                val inflater = requireActivity().layoutInflater
+                with(builder) {
+                    setView(inflater.inflate(R.layout.enter_question, null))
+                        .setPositiveButton(
+                            R.string.action_confirm
+                        ) { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .setNegativeButton(
+                            R.string.action_cancel
+                        ) { dialog, _ ->
+                            dialog.cancel()
+                        }
+                    setOnDismissListener {
+
+                    }
+                    setOnCancelListener {
+
+                    }
+                }
+
+                builder.create()
+            } ?: throw IllegalStateException("Activity cannot be null")
+        }
+
+        companion object {
+
+            fun newInstance(queueId: Long): AddQuestionDialogFragment {
+                val dialog = AddQuestionDialogFragment()
+                dialog.arguments = Bundle().also {
+                    it.putLong(QUEUE_ID_EXTRA, queueId)
+                }
+                return dialog
+            }
+        }
     }
 
     companion object {
