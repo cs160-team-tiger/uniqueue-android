673df12bc6d736a3e0224ca42a340f90f7c8e549
Lai Jiewen <alanljw12345@gmail.com>
2019-11-30 14:49:23 -0800

    Update data model

--- app/src/main/java/tiger/uniqueue/data/model/Question.java
+++ app/src/main/java/tiger/uniqueue/data/model/Question.java
@@ -5,17 +5,27 @@ import com.google.gson.annotations.SerializedName;
 public class Question extends BaseModel {
     @SerializedName("_id")
     public Long id;
+
     @SerializedName("answered_uuid")
     public Long answeredUuid;
+
     @SerializedName("asker_uuid")
     public Long askerUuid;
+
     @SerializedName("assigned_uuid")
     public Long assignedUuid;
+
     @SerializedName("creation_time")
     public Long creationTime;
+
     @SerializedName("question_text")
     public String questionText;
+
     @SerializedName("queue_id")
     public Long queueId;
-    public Long index = -1L;
+
+    @SerializedName("status")
+    public String status;
+
+    public transient Long index = -1L;
 }
